23bffb4b6b80b4d1f4d577a9173a60655f461d2e
Vojislav123 <vojislavalic@hotmail.com>
2023-06-30 20:32:01 +0200

    Update NasaImageGrid.tsx

--- app/imageGalery/NasaImageGrid.tsx
+++ app/imageGalery/NasaImageGrid.tsx
@@ -15,7 +15,7 @@ interface NasaImage {
 
 const NasaImageGrid: React.FC<{ images: NasaImage[] }> = ({ images }) => {
   return (
-    <div className="p-30 mx-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+    <div className="p-30 mx-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 cursor-pointer">
       {images.map((image) => (
         <NasaImageCard key={image.data[0].nasa_id} image={image} />
       ))}
